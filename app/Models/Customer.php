4294946f4ece465412f5e33a9ffb821def99b533
NamaAnda <julianarwansahh@gmail.com>
2025-11-04 09:00:30 +0700

    perbarui model customer

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -65,4 +65,83 @@ class Customer extends Authenticatable
     {
         return $this->hasMany(Cart::class, 'id_customer', 'id_customer');
     }
+
+    /**
+     * Get shipping address components for RajaOngkir
+     */
+    public function getShippingAddressComponents()
+    {
+        return [
+            'city_id' => $this->getCityIdFromAddress(),
+            'province_id' => $this->getProvinceIdFromAddress(),
+            'city_name' => $this->getCityNameFromAddress(),
+            'province_name' => $this->getProvinceNameFromAddress(),
+            'full_address' => $this->alamat,
+        ];
+    }
+    /**
+     * Extract city ID from address
+     * Format yang disarankan: "Jl. Alamat, Kota Jakarta Pusat [CITY:152] [PROVINCE:6]"
+     */
+    public function getCityIdFromAddress()
+    {
+        // Cari pattern [CITY:123]
+        preg_match('/\[CITY:(\d+)\]/', $this->alamat, $matches);
+        return $matches[1] ?? null;
+    }
+
+    /**
+     * Extract province ID from address
+     */
+    public function getProvinceIdFromAddress()
+    {
+        // Cari pattern [PROVINCE:123]
+        preg_match('/\[PROVINCE:(\d+)\]/', $this->alamat, $matches);
+        return $matches[1] ?? null;
+    }
+
+    /**
+     * Extract city name from address
+     */
+    public function getCityNameFromAddress()
+    {
+        // Cari pattern [CITY_NAME:Jakarta Pusat]
+        preg_match('/\[CITY_NAME:(.+?)\]/', $this->alamat, $matches);
+        return $matches[1] ?? null;
+    }
+
+    /**
+     * Extract province name from address
+     */
+    public function getProvinceNameFromAddress()
+    {
+        // Cari pattern [PROVINCE_NAME:DKI Jakarta]
+        preg_match('/\[PROVINCE_NAME:(.+?)\]/', $this->alamat, $matches);
+        return $matches[1] ?? null;
+    }
+
+    /**
+     * Format address untuk menyimpan informasi kota/provinsi
+     */
+    public function formatAddressWithLocation($street, $cityId, $cityName, $provinceId, $provinceName)
+    {
+        return "{$street} [CITY:{$cityId}] [PROVINCE:{$provinceId}] [CITY_NAME:{$cityName}] [PROVINCE_NAME:{$provinceName}]";
+    }
+
+    /**
+     * Get only street address (without location tags)
+     */
+    public function getStreetAddressAttribute()
+    {
+        // Hapus semua tag [TAG:value] dari alamat
+        return preg_replace('/\[[^\]]+\]/', '', $this->alamat) ?? '';
+    }
+
+    /**
+     * Check if address has location data
+     */
+    public function hasLocationData()
+    {
+        return !empty($this->getCityIdFromAddress()) && !empty($this->getProvinceIdFromAddress());
+    }
 }
